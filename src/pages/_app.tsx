46e02c75935634247190e28491bad3e675821fb2
Abbas Mhowwala <abbas@mobiiworld.com>
2024-12-17 18:47:54 +0530

    Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
-import { AuthProvider } from "./components/context/AuthContext";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
