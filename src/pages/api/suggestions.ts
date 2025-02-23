132772ebfdea5105c7a04de422222ef5d0eb2491
Abbas Mhowwala <abbasmhowwala35@gmail.com>
2025-02-23 13:54:34 +0530

    Google Places API

--- src/pages/api/suggestions.ts
+++ src/pages/api/suggestions.ts
@@ -1,10 +1,17 @@
-// pages/api/suggestions.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   const { query } = req.query;
-
+  if (!query) {
+    return res.status(400).json({ message: 'Query parameter is required' });
+  }
   try {
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_SUGGESTION_API_URL}?input=${query}&types=(cities)&key=${process.env.NEXT_PUBLIC_API_KEY}`
